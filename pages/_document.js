091d72aa717d5a32d4239f0d4f70ae05da0c3998
Alan Duong <alanduong07@gmail.com>
2022-09-23 22:22:31 -0500

    first commit for AFTER PROMPTS RELEASED
    Initialized Next.js, Supabase, Tailwind.css

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+} from "next/document";
+
+// A special Next.js component that is used to initialize pages. 
+// It's common to override this component to add global CSS or 
+// other elements that are needed on every page to the "head" of the HTML document.
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
