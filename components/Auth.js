c5cd362f97ee9359b4fb11538d22c6ce4d0f4c93
Alan Duong <alanduong07@gmail.com>
2022-09-25 11:00:27 -0500

    allow hackathon judges to try live app

--- components/Auth.js
+++ components/Auth.js
@@ -55,7 +55,8 @@ export default function Auth() {
             setLoading(true);
             // If the email is not a Rice email, throw an error
             if (!email.endsWith("@rice.edu")) {
-                throw new Error("Please use a Rice email");
+                // throw new Error("Please use a Rice email");
+                toast.warn("Usually we'd ask for a university email only, but for Hackathon judges, you can try out the app now!")
             }
             const { error } = await supabase.auth.signInWithOtp({ email });
             if (error) throw error;
